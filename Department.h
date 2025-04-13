2599109326c86b1c9696385ba3cce97bc4e9ba08
Valiqulova Durdona <d.valiqulova@newuu.uz>
2025-04-13 16:20:53 +0500

    D

--- Department.h
+++ Department.h
@@ -4,24 +4,57 @@
 
 #ifndef DEPARTMENT_H
 #define DEPARTMENT_H
-#include <vector>
+
+#include "Applicant.h"
+#include <algorithm>
 #include <memory>
+#include <vector>
 using namespace std;
 
-template <typename T>
-class Department {
+template <typename T> class Department {
 private:
-    vector<shared_ptr<T>> employees;
+  vector<shared_ptr<T>> members;
+  vector<Applicant> applicants;
+
 public:
-    void addEmployee(T* emp) {
-        employees.push_back(shared_ptr<T>(emp));
+  void addEmployee(shared_ptr<T> emp) { members.push_back(emp); }
+
+  void addApplicant(const Applicant &app) {
+    if (!app.isQualified()) {
+      throw runtime_error("Applicant doesn't meet minimum requirements");
     }
-    void showEmployees() const {
-        for (const auto& emp : employees) {
-            emp->display();
-        }
+    applicants.push_back(app);
+  }
+
+  template <typename EmployeeType>
+  shared_ptr<EmployeeType> hireApplicant(int applicantIndex, int newEmployeeId,
+                                         double salary) {
+    if (applicantIndex < 0 || applicantIndex >= applicants.size()) {
+      throw out_of_range("Invalid applicant index");
     }
+
+    Applicant &app = applicants[applicantIndex];
+    auto newEmployee = make_shared<EmployeeType>(app.getName(), app.getAge(),
+                                                 newEmployeeId, salary);
+
+    addEmployee(newEmployee);
+    applicants.erase(applicants.begin() + applicantIndex);
+    return newEmployee;
+  }
+
+  void displayApplicants() const {
+    cout << "--- Applicants ---\n";
+    for (const auto &app : applicants) {
+      app.display();
+    }
+  }
+
+  void showEmployees() const {
+    cout << "--- Employees ---\n";
+    for (const auto &emp : members) {
+      emp->display();
+    }
+  }
 };
 
-
-#endif //DEPARTMENT_H
+#endif // DEPARTMENT_H
